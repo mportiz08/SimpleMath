92b7befc00a334f92cc043ff06e3d98216808e16
Marcus Ortiz <mportiz08@gmail.com>
2011-06-02 21:36:03 -0700

    added scrolling

--- javascript/demo-mode.js
+++ javascript/demo-mode.js
@@ -1,3 +1,20 @@
 $(document).ready(function() {
-  console.log("testing");
+  $('#nav-welcome').click(function() {
+    $.scrollTo('#welcome', 2500);
+  });
+  $('#nav-events').click(function() {
+    $.scrollTo('#events', 2500);
+  });
+  $('#nav-tweets').click(function() {
+    $.scrollTo('#tweets', 2500);
+  });
+  $('#nav-lyrics').click(function() {
+    $.scrollTo('#lyrics', 2500);
+  });
+  $('#nav-photos').click(function() {
+    $.scrollTo('#photos', 2500);
+  });
+  $('#nav-videos').click(function() {
+    $.scrollTo('#videos', 2500);
+  });
 });
